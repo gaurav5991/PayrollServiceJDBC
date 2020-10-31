c2f02d60adfe5fafb725d7a03348a5e406fce4c5
gaurav5991 <gauravharendersharma@gmail.com>
2020-10-30 22:56:03 -0700

    Gaurav[ADDED]:Welcome Message

---
+++ src/main/java/com/bridgelabz/employeepayroll/DemoDB.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayroll;
+
+public class DemoDB {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll Database System");
+    }
+}
